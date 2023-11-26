28bd129ad5b84da439f7634316dcc7d34dc5cff3
gre-v-el <myszkier.gabriel@gmail.com>
2023-11-26 09:09:49 +0100

    Deleted rule struct, simplified rule choice.

--- src/l_system.rs
+++ src/l_system.rs
@@ -1,15 +1,15 @@
 use std::collections::VecDeque;
 
-use crate::{grid::Grid, rule::Rule, cell::Cell};
+use crate::{grid::Grid, cell::Cell};
 
 pub struct LSystem<const N: usize> {
 	state: Grid,
-	rules: [Rule; N],
+	rules: [Grid; N],
 	stem_queue: VecDeque<[isize; 2]>,
 }
 
 impl<const N: usize> LSystem<N> {
-	pub fn new(state: Grid, rules: [Rule; N]) -> Self {
+	pub fn new(state: Grid, rules: [Grid; N]) -> Self {
 		let mut stem_queue = VecDeque::new();
 		
 		for ([x, y], cell) in &state {
@@ -32,33 +32,23 @@ impl<const N: usize> LSystem<N> {
 		let pos = self.stem_queue.pop_front().unwrap();
 
 		if let Cell::Stem(stem_type, stem_dir) = self.state.at(pos) {
-			let mut matched = None;
+			let to = &self.rules[stem_type as usize];
 
-			for rule in &self.rules {
-				if rule.from == stem_type {
-					matched = Some(&rule.to);
-					break;
-				}
-			}
+			self.state.insert(to, pos, stem_dir);
 
-			if let Some(to) = matched {
-				self.state.insert(to, pos, stem_dir);
+			self.stem_queue.retain(|e| {
+				!to.contains(self.state.pos_to_other_pos(*e, pos))
+			});
 
-				self.stem_queue.retain(|e| {
-					!to.contains(self.state.pos_to_other_pos(*e, pos))
-				});
-
-				for (other_pos, cell) in to {
-					match cell {
-						Cell::Stem(_, _) => {
-							let other_pos = stem_dir.rotate_coords(other_pos[0], other_pos[1]);
-							let state_pos = to.pos_to_other_pos(other_pos, [-pos[0], -pos[1]]);
-							self.stem_queue.push_back(state_pos);
-						}
-						_ => {}
+			for (other_pos, cell) in to {
+				match cell {
+					Cell::Stem(_, _) => {
+						let other_pos = stem_dir.rotate_coords(other_pos[0], other_pos[1]);
+						let state_pos = to.pos_to_other_pos(other_pos, [-pos[0], -pos[1]]);
+						self.stem_queue.push_back(state_pos);
 					}
+					_ => {}
 				}
-
 			}
 		}
 		else {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-mod rule;
 mod grid;
 mod l_system;
 mod cell;
@@ -8,7 +7,6 @@ use std::env;
 use cell::Cell::*;
 use grid::Grid;
 use l_system::LSystem;
-use rule::Rule;
 
 use crate::cell::Direction;
 
@@ -19,15 +17,15 @@ fn main() {
     let mut system = LSystem::new(
 		Grid::single(Stem(0, Direction::UP)),
 		[
-			Rule::new(0, Grid::new(3, 3, &[
+			Grid::new(3, 3, &[
 					Passive, Passive, Stem(1, Direction::DOWN),
 					Passive, Empty, Empty,
 					Stem(2, Direction::LEFT), Empty, Empty,
 				],
 				[0, 0]
-			)),
-			Rule::new(1, Grid::vertical  (&[Stem(2, Direction::RIGHT), Passive, Passive], 2)),
-			Rule::new(2, Grid::vertical  (&[Stem(1, Direction::LEFT), Passive, Passive], 2)),
+			),
+			Grid::vertical  (&[Stem(2, Direction::RIGHT), Passive, Passive], 2),
+			Grid::vertical  (&[Stem(1, Direction::LEFT), Passive, Passive], 2),
 		],
 	);
 
