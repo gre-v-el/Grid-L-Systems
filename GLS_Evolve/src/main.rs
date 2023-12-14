f0c7cf628359f4cafd0d92fe45569d5d45ceebe2
gre-v-el <myszkier.gabriel@gmail.com>
2023-12-14 15:35:29 +0100

    Split the project into workspaces.

--- GLS_Evolve/src/main.rs
+++ GLS_Evolve/src/main.rs
@@ -15,27 +15,6 @@ use state::State;
 #[macroquad::main("Grid L-systems")]
 async fn main() {
 	env::set_var("RUST_BACKTRACE", "1");
-	
-	// let mut alg: GeneticAlgorithm<LS> = GeneticAlgorithm::new(150, 40, 1.0);
-	// for i in 0..1000 {
-	// 	if i % 100 == 0 { println!("gen {i}/1000"); }
-	// 	alg.perform_generation();
-	// }
-	// let mut best = alg.best().0.clone();
-	// best.reset();
-	// let mut best = best.0;
-	
-	
-	// for (i, rule) in best.rules().iter().enumerate() {
-	// 	println!("{i}\nV\n{rule}");
-	// }
-
-	// println!("{}", "\n".repeat(5));
-	
-	// for _ in 0..25 {
-	// 	println!("{}", best.state());
-	// 	best.try_step();
-	// }
 
 	egui_macroquad::cfg(|ctx| {
 		ctx.set_pixels_per_point(1.2);
@@ -55,7 +34,6 @@ async fn main() {
 
 
 /*
-split the project into workspaces
 save/load
 smooth killing
 fitness optimizations
